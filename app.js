3609dfa21315c90abc14554ac321fb0ceb9753fd
Kawsar Alam khan <kawsarjahin@gmail.com>
2023-10-24 19:35:15 +0100

    Space sorting updated with added comments in html

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ new Vue({
     },
     validName: true,
     validPhone: true,
-    orderBySpace: "ascending", // Initialize the sorting order
+    orderBySpace: "ascending",
   },
   created() {
     fetch("products.json")
@@ -60,16 +60,20 @@ new Vue({
         }
       }
     },
-    
-    
+    sortProductsBySpaceAscending() {
+      // Sort products by space in ascending order
+      this.orderBySpace = "ascending";
+      this.products.sort((a, b) => a.available - b.available);
+    },
+
     toggleCart() {
       this.showCart = !this.showCart;
     },
     validateName() {
-      this.validName = true; // You can add validation logic for the name field here
+      this.validName = true; 
     },
     validatePhone() {
-      this.validPhone = true; // You can add validation logic for the phone field here
+      this.validPhone = true; 
     },
     placeOrder() {
       alert("Order has been submitted.");
@@ -96,7 +100,7 @@ new Vue({
     },
 
     toggleOrderBySpace() {
-      // Toggle the sorting order when clicking the button
+      // Toggles the sorting order when the button is clicked
       if (this.orderBySpace === "ascending") {
         this.orderBySpace = "descending";
       } else {
@@ -104,16 +108,11 @@ new Vue({
       }
     },
 
-   toggleCart: function () {
+    toggleCart: function () {
       this.showCart = !this.showCart;
     },
   },
 });
-
-
-
-
-
 
 
 function search() {
@@ -121,59 +120,61 @@ function search() {
   var searchTerm = document.getElementById("searchInput").value.toLowerCase();
   var searchResults = document.getElementById("searchResults");
 
-  // Define a map or object to store search results
+  //Object to store search results
   var searchResultData = {
-    "a": "Subject: Math  Location: London <br> Subject: Bengali Location: Dhaka <br> Subject: Chemistry Location: Wales",
-    "b": "Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: Biology Location: London",
-    "c": "Subject: Sculpture Location: Cork <br> Subject: Chemistry Location: Wales <br> Subject: French Location: London <br> Subject: Cooking Location: London",
-    "d": "Subject: Bengali <br> Location: Dhaka",
-    "e": "Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Sculpture Location: Cork <br> Subject: Chemistry Location: Wales",
-    "f": "Subject: English Location: Oxford <br> Subject: French Location: London",
-    "g": "Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: Biology Location: London <br> Subject: Cooking Location: London",
-    "h": "Subject: Math Location: London <br> Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: French Location: London",
-    "i": "Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: Music Location: Troy <br> Subject: Chemistry Location: Wales <br> Subject: Biology Location: London <br> Subject: Cooking Location: London",
-    "k": "Subject: Sculpture Location: Cork <br> Subject: Cooking Location: London",
-    "l": "Subject: Math Location: London <br> Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Sculpture Location: Cork  <br> Subject: Chemistry Location: Wales <br> Subject: Biology Location: London <br> Subject: French Location: London <br> Subject: Cooking Location: London",
-    "m": "Subject: Math Location: London <br> Subject: Music Location: Troy <br> Subject: Chemistry Location: Wales",
-    "n": "Subject: Math Location: London <br> Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: Biology Location: London <br> Subject: French Location: London <br> Subject: Cooking Location: London",
-    "0": "Subject: Math Location: London <br> Subject: English Location: Oxford <br> Subject: Physics Location: Brighton <br> Subject: Music Location: Troy <br> Subject: Sculpture Location: Cork <br> Subject: Biology Location: London <br> Subject: French Location: London <br> Subject: Cooking Location: London",
-    "p": "Subject: Physics Location: Brighton <br> Subject: Sculpture Location: Cork",
-    "r": "Subject: English Location: Oxford <br> Subject: Music Location: Troy <br> Subject: Sculpture Location: Cork <br> Subject: Chemistry Location: Wales <br> Subject: French Location: London",
-    "s": "Subject: Physics Location: Brighton <br> Subject: Music Location: Troy <br> Subject: Sculpture Location: Cork <br> Subject: Chemistry Location: Wales",
-    "t": "Subject: Math Location: London <br> Subject: Physics Location: Brighton <br> Subject: Music Location: Troy <br> Subject: Chemistry Location: Wales",
-    "u": "Subject: Music Location: Troy <br> Subject: Sculpture Location: Cork",
-    "w": "Subject: Chemistry <br> Location: Wales",
-    "x": "Subject: English Location: Oxford",
-    "math": "Subject: Math <br> Location: London",
-    "english": "Subject: English <br> Location: Oxford",
-    "bengali": "Subject: Bengali <br> Location: Dhaka",
-    "physics": "Subject: Physics <br> Location: Brighton",
-    "music": "Subject: Music <br> Location: Troy",
-    "sculpture": "Subject: Sculpture<br>  Location: Cork",
-    "chemistry": "Subject: Chemistry <br> Location: Melbourne",
-    "biology": "Subject: Biology <br> Location: London",
-    "french":  "Subject: French <br> Location: London",
-    "cooking":  "Subject: Cooking <br> Location: London",
-    "50":     " Subject: Math Location:London <br> Subject:Bengali Location:Dhaka <br> Subject:Physics Location:Brighton",
-    "60":  " Subject:Music Location:Troy <br> Subject:Sculpture Location:Cork <br> Subject:Chemistry Location:Wales <br> Subject:Biology Location:London ",
-    "70" : "Subject:French Location:London <br>  Subject:English Location:Oxford",
-    "90":  " Subject:French Location:London <br>  Subject:Cooking Location:London",
+    a: "Subject: Math  Location: London <br> Subject: Bengali Location: Dhaka <br> Subject: Chemistry Location: Wales",
+    b: "Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: Biology Location: London",
+    c: "Subject: Sculpture Location: Cork <br> Subject: Chemistry Location: Wales <br> Subject: French Location: London <br> Subject: Cooking Location: London",
+    d: "Subject: Bengali <br> Location: Dhaka",
+    e: "Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Sculpture Location: Cork <br> Subject: Chemistry Location: Wales",
+    f: "Subject: English Location: Oxford <br> Subject: French Location: London",
+    g: "Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: Biology Location: London <br> Subject: Cooking Location: London",
+    h: "Subject: Math Location: London <br> Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: French Location: London",
+    i: "Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: Music Location: Troy <br> Subject: Chemistry Location: Wales <br> Subject: Biology Location: London <br> Subject: Cooking Location: London",
+    k: "Subject: Sculpture Location: Cork <br> Subject: Cooking Location: London",
+    l: "Subject: Math Location: London <br> Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Sculpture Location: Cork  <br> Subject: Chemistry Location: Wales <br> Subject: Biology Location: London <br> Subject: French Location: London <br> Subject: Cooking Location: London",
+    m: "Subject: Math Location: London <br> Subject: Music Location: Troy <br> Subject: Chemistry Location: Wales",
+    n: "Subject: Math Location: London <br> Subject: English Location: Oxford <br> Subject: Bengali Location: Dhaka <br> Subject: Physics Location: Brighton <br> Subject: Biology Location: London <br> Subject: French Location: London <br> Subject: Cooking Location: London",
+    0: "Subject: Math Location: London <br> Subject: English Location: Oxford <br> Subject: Physics Location: Brighton <br> Subject: Music Location: Troy <br> Subject: Sculpture Location: Cork <br> Subject: Biology Location: London <br> Subject: French Location: London <br> Subject: Cooking Location: London",
+    p: "Subject: Physics Location: Brighton <br> Subject: Sculpture Location: Cork",
+    r: "Subject: English Location: Oxford <br> Subject: Music Location: Troy <br> Subject: Sculpture Location: Cork <br> Subject: Chemistry Location: Wales <br> Subject: French Location: London",
+    s: "Subject: Physics Location: Brighton <br> Subject: Music Location: Troy <br> Subject: Sculpture Location: Cork <br> Subject: Chemistry Location: Wales",
+    t: "Subject: Math Location: London <br> Subject: Physics Location: Brighton <br> Subject: Music Location: Troy <br> Subject: Chemistry Location: Wales",
+    u: "Subject: Music Location: Troy <br> Subject: Sculpture Location: Cork",
+    w: "Subject: Chemistry <br> Location: Wales",
+    x: "Subject: English Location: Oxford",
+    math: "Subject: Math <br> Location: London",
+    english: "Subject: English <br> Location: Oxford",
+    bengali: "Subject: Bengali <br> Location: Dhaka",
+    physics: "Subject: Physics <br> Location: Brighton",
+    music: "Subject: Music <br> Location: Troy",
+    sculpture: "Subject: Sculpture<br>  Location: Cork",
+    chemistry: "Subject: Chemistry <br> Location: Melbourne",
+    biology: "Subject: Biology <br> Location: London",
+    french: "Subject: French <br> Location: London",
+    cooking: "Subject: Cooking <br> Location: London",
+    50: " Subject: Math Location:London <br> Subject:Bengali Location:Dhaka <br> Subject:Physics Location:Brighton",
+    60: " Subject:Music Location:Troy <br> Subject:Sculpture Location:Cork <br> Subject:Chemistry Location:Wales <br> Subject:Biology Location:London ",
+    70: "Subject:French Location:London <br>  Subject:English Location:Oxford",
+    90: " Subject:French Location:London <br>  Subject:Cooking Location:London",
   };
 
-  if (searchTerm === searchResults.getAttribute('data-search-term')) {
+  if (searchTerm === searchResults.getAttribute("data-search-term")) {
     // If the search term matches the one in the results, clear the results and input field
     searchResults.innerHTML = "";
     document.getElementById("searchInput").value = "";
-    searchResults.removeAttribute('data-search-term');
+    searchResults.removeAttribute("data-search-term");
   } else {
     // Check if the search term is in the searchResultData object
     if (searchResultData.hasOwnProperty(searchTerm)) {
       // Display the search result
       searchResults.innerHTML = searchResultData[searchTerm];
-      searchResults.setAttribute('data-search-term', searchTerm);
+      searchResults.setAttribute("data-search-term", searchTerm);
     } else {
       searchResults.innerHTML = "No results found";
-      searchResults.setAttribute('data-search-term', searchTerm);
+      searchResults.setAttribute("data-search-term", searchTerm);
     }
   }
 }
+
+
